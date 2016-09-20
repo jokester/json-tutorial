4fdb071b73ce05b7d95bab72aeb53158891f1d03
Wang Guan <momocraft@gmail.com>
2016-09-20 16:23:10 +0000

    tutorial 01: fix 'LEPT_PARSE_ROOT_NOT_SINGULAR' case

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -2,12 +2,22 @@
 #include <assert.h>  /* assert() */
 #include <stdlib.h>  /* NULL */
 
+/**
+ * @param c lept_context
+ * @param ch char
+ */
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
 
+/**
+ * `context': internal state of a parse
+ */
 typedef struct {
     const char* json;
 }lept_context;
 
+/**
+ * skip to next non-space character
+ */
 static void lept_parse_whitespace(lept_context* c) {
     const char *p = c->json;
     while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
@@ -19,11 +29,14 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     EXPECT(c, 'n');
     if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
         return LEPT_PARSE_INVALID_VALUE;
+    /* only modifies state when a "null" is found */
     c->json += 3;
     v->type = LEPT_NULL;
     return LEPT_PARSE_OK;
 }
 
+/**
+ */
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
         case 'n':  return lept_parse_null(c, v);
@@ -32,13 +45,34 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
     }
 }
 
+/**
+ * facade: skip whitespace and return first parse result?
+ */
 int lept_parse(lept_value* v, const char* json) {
     lept_context c;
+    int parse1;
     assert(v != NULL);
     c.json = json;
+    /* FIXME why? */
     v->type = LEPT_NULL;
+    /* skip white space */
     lept_parse_whitespace(&c);
-    return lept_parse_value(&c, v);
+    parse1 = lept_parse_value(&c, v);
+
+    if (parse1 != LEPT_PARSE_OK) {
+        return parse1;
+    }
+
+    /**
+     * lept_parse_whitespace again,
+     * and throw LEPT_PARSE_ROOT_NOT_SINGULAR if another value is found
+     */
+    lept_parse_whitespace(&c);
+    if (! *c.json) {
+        return parse1;
+    } else {
+        return LEPT_PARSE_ROOT_NOT_SINGULAR;
+    }
 }
 
 lept_type lept_get_type(const lept_value* v) {
